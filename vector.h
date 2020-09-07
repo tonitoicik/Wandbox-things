0b62877bc861970b9fe6359074ae5c557d0f2ffd
tonitoicik <54882998+tonitoicik@users.noreply.github.com>
2020-09-07 21:50:06 +0300

    Create vector.h

---
+++ vector.h
@@ -0,0 +1,12 @@
+class Vector { 
+    protected: 
+        int _x;
+        int _y;
+    public:
+        Vector(int x, int y); 
+            int getX();
+            int getY();
+            void getCoords();
+            void getSqare();
+            double getAngle();
+             };
