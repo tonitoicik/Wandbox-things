234ac26155ad6a68502343401ed2f90a661e585d
tonitoicik <54882998+tonitoicik@users.noreply.github.com>
2020-09-07 21:48:03 +0300

    Create point.cpp

---
+++ point.cpp
@@ -0,0 +1,13 @@
+#include "point.h"
+Point::Point(double x , double y){
+            _x = x;
+            _y = y;
+            }
+double Point::getX()
+    {
+    return _x;
+    }
+double Point::getY()
+    {
+    return _y;
+    }
