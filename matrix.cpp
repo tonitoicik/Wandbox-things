89f0493bc9befc38624be3497a5defd3b277e82a
tonitoicik <54882998+tonitoicik@users.noreply.github.com>
2020-09-07 21:58:40 +0300

    Update matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -27,3 +27,20 @@ void Matrix2::mulR(double f[2][2])
              }
                 
         };
+void Matrix2::mulL(double f[2][2])
+        {
+        double result[2][2];
+        result[0][0]=(_m[0][0]*f[0][0])+(_m[1][0]*f[0][1]);
+        result[0][1]=(_m[0][1]*f[0][0])+(_m[1][1]*f[0][1]);
+        result[1][0]=(_m[0][0]*f[1][0])+(_m[1][0]*f[1][1]);
+        result[1][1]=(_m[0][1]*f[1][0])+(_m[1][1]*f[1][1]);
+        for (int i=0; i<2; i++)
+            {
+                for (int j=0; j<2; j++)
+                    {
+                        std::cout<< result[i][j] << " " ;
+                    }
+               std::cout<< std::endl;
+             }
+                
+        };    
